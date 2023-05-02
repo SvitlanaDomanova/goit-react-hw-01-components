a3053c323278db37f91cdf13cef76d3b6a0ff3db
SvitlanaDomanova <majba161@gmail.com>
2023-05-02 07:26:37 +0300

    finally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from './Profile/profile';
+import user from '../data/user.json';
+
+import { Transactions } from './Transactions/Transactions';
+import transactions from '../data/transactions.json';
+
+import {Statistics} from './Statistics/Statistics';
+import data from 'data/statistics.json';
+
+import { Friends } from './Friends/Friends';
+import friends from 'data/friends.json';
+
+
+export default function App() {
+    return (
+      <>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Transactions items={transactions} />
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+  
+        <Friends items={friends} />
+      </>
+    );
+  }
+  
